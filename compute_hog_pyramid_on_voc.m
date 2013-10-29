2667c242d5a61a996426221e225be73b5265e411
Chun-Wei Liu <chunwei@cs.unc.edu>
2013-10-29 10:03:21 -0400

    Compute HOG pyramid and store in local directory
    HOG pyramid is from examplarsvm. Looping on VOC is from
    example_detector in the VOC development kit.

---
+++ compute_hog_pyramid_on_voc.m
@@ -0,0 +1,89 @@
+function compute_hog_pyramid_on_voc
+
+% change this path if you install the VOC code elsewhere
+addpath([cd '/VOCcode']);
+addpath([cd '/features']);
+
+% initialize VOC options
+VOCinit;
+
+% initialize parameters
+params=get_default_params;
+%params.feature_type='hog';
+
+% compute feature for each class
+for i=1:VOCopts.nclasses
+    
+    cls=VOCopts.classes{i};
+    compute_hog_pyramid(VOCopts,cls,params);
+    
+end
+
+function compute_hog_pyramid(VOCopts,cls,params)
+
+% load training set
+
+cp=sprintf(VOCopts.annocachepath,VOCopts.trainset);
+if exist(cp,'file')
+    fprintf('%s: loading training set\n',cls);
+    load(cp,'gtids','recs');
+else
+    tic;
+    gtids=textread(sprintf(VOCopts.imgsetpath,VOCopts.trainset),'%s');
+    for i=1:length(gtids)
+        % display progress
+        if toc>1
+            fprintf('%s: load: %d/%d\n',cls,i,length(gtids));
+            drawnow;
+            tic;
+        end
+
+        % read annotation
+        recs(i)=PASreadrecord(sprintf(VOCopts.annopath,gtids{i}));
+    end
+    save(cp,'gtids','recs');
+end
+
+% compute pyramid for each training image
+
+tic;
+for i=1:length(gtids)
+    % display progress
+    if toc>1
+        fprintf('%s: train: %d/%d\n',cls,i,length(gtids));
+        drawnow;
+        tic;
+    end
+    
+    % find objects of class and extract difficult flags for these objects
+    clsinds=strmatch(cls,{recs(i).objects(:).class},'exact');
+    diff=[recs(i).objects(clsinds).difficult];
+    
+    % assign ground truth class to image
+    if isempty(clsinds)
+        gt=-1;          % no objects of class
+    elseif any(~diff)
+        gt=1;           % at least one non-difficult object of class
+    else
+        gt=0;           % only difficult objects
+    end
+
+    if gt
+        % extract features for image
+        fdp=sprintf(VOCopts.exfdpath,gtids{i});
+        if exist(fdp,'file')
+            % load features
+            load(fdp,'fd');
+        else
+            % compute and save features
+            I=imread(sprintf(VOCopts.imgpath,gtids{i}));
+            fd=extractfd(I,params);
+            save(fdp,'fd');
+        end
+    end
+end
+
+function fd = extractfd(I,params)
+fd=esvm_pyramid(double(I),params);
+
+
